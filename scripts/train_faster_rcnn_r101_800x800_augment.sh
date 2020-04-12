4c3ab7f7902325d06bb472d09ef63718a748c020
“Lukas <>
2020-04-13 01:12:55 +0200

    Updating the best script

--- scripts/train_faster_rcnn_r101_800x800_augment.sh
+++ scripts/train_faster_rcnn_r101_800x800_augment.sh
@@ -2,8 +2,8 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-CHECKPOINT_DIR=../checkpoints/pipeline_faster_rcnn_resnet101_800x800_inat_augment
-CONFIG_PATH=../configs/pipeline_faster_rcnn_resnet101_800x800_inat_augment.config
+CHECKPOINT_DIR=../checkpoints/pipeline_faster_rcnn_resnet101_800x800_augment
+CONFIG_PATH=../configs/pipeline_faster_rcnn_resnet101_800x800_augment.config
 CHECKPOINT_STEPS=500
 
 python ../object_detection/model_main.py \

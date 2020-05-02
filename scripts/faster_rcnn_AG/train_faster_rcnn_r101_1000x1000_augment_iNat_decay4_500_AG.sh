b97fe0565845f72707d9f3100c6530861790e8c2
“Lukas <>
2020-05-02 22:01:02 +0200

    Adding best config Faster-Rcnn.

---
+++ scripts/faster_rcnn_AG/train_faster_rcnn_r101_1000x1000_augment_iNat_decay4_500_AG.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export CUDA_VISIBLE_DEVICES=7
+
+CHECKPOINT_DIR=../../checkpoints/faster_rcnn_resnet101_1000x1000_augment_iNat_decay4_500
+CONFIG_PATH=../../configs/faster_rcnn_ag/pipeline_faster_rcnn_resnet101_1000x1000_augment_iNat_decay4_500_AG.config
+CHECKPOINT_STEPS=100
+
+python ../object_detection/model_main.py \
+    --model_dir "$CHECKPOINT_DIR" \
+    --pipeline_config_path "$CONFIG_PATH" \
+    --checkpoint_steps "$CHECKPOINT_STEPS"

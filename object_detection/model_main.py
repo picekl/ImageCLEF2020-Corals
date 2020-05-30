3f8671783e78c1fe2f1e4c2ee0813b67ec7ea9f8
Antonin Riha <antonin.riha@bluedynamic.cz>
2020-05-30 14:14:56 +0200

    changed max number of checkpoints

--- object_detection/model_main.py
+++ object_detection/model_main.py
@@ -63,7 +63,7 @@ def main(unused_argv):
 
   flags.mark_flag_as_required('model_dir')
   flags.mark_flag_as_required('pipeline_config_path')
-  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, session_config=session_config, save_checkpoints_steps=FLAGS.checkpoint_steps, save_checkpoints_secs=None)
+  config = tf.estimator.RunConfig(model_dir=FLAGS.model_dir, session_config=session_config, save_checkpoints_steps=FLAGS.checkpoint_steps, save_checkpoints_secs=None, keep_checkpoint_max=1000)
 
   train_and_eval_dict = model_lib.create_estimator_and_inputs(
       run_config=config,

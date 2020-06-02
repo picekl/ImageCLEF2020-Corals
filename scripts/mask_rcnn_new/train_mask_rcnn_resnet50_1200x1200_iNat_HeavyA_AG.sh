4d8dd29597148de8f7b7fe58d85cd094c8d04b69
“Lukas <Lukaspicek@gmail.com>
2020-06-02 15:27:43 +0200

    Coral detection - last phase scripts.

---
+++ scripts/mask_rcnn_new/train_mask_rcnn_resnet50_1200x1200_iNat_HeavyA_AG.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export CUDA_VISIBLE_DEVICES=1
+
+CHECKPOINT_DIR=../../checkpoints/mask_rcnn_resnet50_1200x1200_iNat_HeavyA_AG
+CONFIG_PATH=../../configs/mask-rcnn-new/mask_rcnn_resnet50_1200x1200_iNat_HeavyA_AG.config
+CHECKPOINT_STEPS=1950 #5 epochs
+
+python ../../object_detection/model_main.py \
+    --model_dir "$CHECKPOINT_DIR" \
+    --pipeline_config_path "$CONFIG_PATH" \
+    --checkpoint_steps "$CHECKPOINT_STEPS"
